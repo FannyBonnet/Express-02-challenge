fc05560f8b31994b3c610172d92b53cdb9171955
FannyBonnet <fanny.bonnet@sncf.fr>
2023-01-24 12:20:31 +0100

    challenge 05 finished

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ app.post("/api/users", userHandlers.postUser);
 
 app.put("/api/users/:id", userHandlers.updateUser);
 
+app.delete("/api/users/:id", userHandlers.deleteUser);
+
 app.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
